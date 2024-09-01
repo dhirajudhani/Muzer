8f31f7d7d15e4bc848dadfdab98ec2b62277a904
dhirajudhani <dhirajudhani870@gmail.com>
2024-09-01 19:35:08 +0530

    created landing page

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -1,18 +1,21 @@
 "use client";
-
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+//@ts-ignore
+import { Music } from "lucide-react"
 
 export function Appbar() {
     const session = useSession();
 
-    return <div>
-        <div className="flex justify-between">
-            <div> Muzer</div>
-            <div>
-                {session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => signOut()}>Logout</button>}
-                {!session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => signIn()}>SignIn</button>}
-            </div>
+    return <div className="flex justify-between px-20 pt-4">
+        <div className="text-lg font-bold flex flex-col justify-center text-white">
+            Muzer
         </div>
-       
+        <div>
+            {session.data?.user && <Button className="bg-purple-600 text-white hover:bg-purple-700" onClick={() => signOut()}>Logout</Button>}
+            {!session.data?.user &&<Button className="bg-purple-600 text-white hover:bg-purple-700" onClick={() => signIn()}>Signin</Button>}
+        </div>
     </div>
 }
